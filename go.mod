968604f5d561833d30be3cc8bcb496431dc155c1
Chuck Chau <chuck.chau@pluto.tv>
2023-03-13 12:24:08 -0700

    init repo

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/volkstrader/dating
+
+go 1.20
+
+require (
+	github.com/volkstrader/actress v1.0.0
+	github.com/volkstrader/model v1.0.1
+)
+
+require github.com/volkstrader/girlfriend v1.0.1 // indirect
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"github.com/volkstrader/actress"
+	"github.com/volkstrader/model"
+)
+
+func main() {
+	const name = "barbie"
+	const age = 25
+
+	a := actress.Profile{
+		Name: name,
+		Age:  age,
+	}
+
+	m := model.Profile{
+		Name: name,
+		Age:  age,
+	}
+
+	dest, ok := bookVacation(a)
+	if !ok {
+		fmt.Printf("not dating with actress %s\n", a.Name)
+	} else {
+		fmt.Printf("going to %s with actress %s\n", dest, a.Name)
+	}
+
+	dest, ok = bookVacation(m)
+	if !ok {
+		fmt.Printf("not dating with model %s\n", a.Name)
+	} else {
+		fmt.Printf("going to %s with model %s\n", dest, a.Name)
+	}
+
+}
+
+type partner interface {
+	Dating() bool
+}
+
+func bookVacation(p partner) (string, bool) {
+	if !p.Dating() {
+		return "", false
+	}
+
+	return "Hawaii", true
+}
